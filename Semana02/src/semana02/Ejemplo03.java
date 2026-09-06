e8f98f0ff8e3cc58cbde94cf17254b1c4fcd4cf7
ivanarc11 <ivan.a.rcalvo@gmail.com>
2026-09-05 20:01:04 -0500

    Cambio realizado en ivan-cambiosV1

--- Semana02/src/semana02/Ejemplo03.java
+++ Semana02/src/semana02/Ejemplo03.java
@@ -13,16 +13,17 @@ public class Ejemplo03 {
      /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         int[] arreglo1 = new int[4];
-        
+
         arreglo1[0] = 10;
         arreglo1[1] = 1;
         arreglo1[2] = 3;
         arreglo1[3] = -8;
-        
-        for(int i=0; i<arreglo1.length; i++){
-            System.out.print(arreglo1[i] + "t");
+
+        for (int i = 0; i < arreglo1.length; i++) {
+            // Cambio realizado: se agregó "\t" para separar los números con una tabulación.
+            System.out.print(arreglo1[i] + "\t");
         }
     }
     
