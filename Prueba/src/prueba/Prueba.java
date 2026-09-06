0c24c0bb5132b15c0e63e2f7a38c57c64d1a9320
Jesus <jesus17072003@gmail.com>
2026-09-06 15:06:26 -0500

    Cambio para conflicto en main

--- Prueba/src/prueba/Prueba.java
+++ Prueba/src/prueba/Prueba.java
@@ -14,7 +14,7 @@ public class Prueba {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here`r`n        System.out.println("Cambio realizado por Jesus");
+        // TODO code application logic here`r`n        System.out.println("Cambio realizado desde main");
     }
     
 }
