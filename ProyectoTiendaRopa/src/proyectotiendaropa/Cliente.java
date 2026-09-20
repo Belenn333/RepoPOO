3f8854b03d6d531683c1a55328a72cd988acf234
Belenn333 <kiaraferh@gmail.com>
2026-09-20 02:07:07 -0500

    Creando los atributos iniciales de la clase Cliente

---
+++ ProyectoTiendaRopa/src/proyectotiendaropa/Cliente.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectotiendaropa;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Cliente {
+    //Declaracion de atributos Clase Cliente
+    private String codigo;
+    private String nombre;
+    private String dni;
+    private String telefono;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+}
--- ProyectoTiendaRopa/src/proyectotiendaropa/Ropa.java
+++ ProyectoTiendaRopa/src/proyectotiendaropa/Ropa.java
@@ -9,5 +9,70 @@ package proyectotiendaropa;
  * @author USUARIO
  */
 public class Ropa {
+    //Declaracion de atributos Clase Ropa
+    private String codigo;
+    private String nombre;
+    private String talla;
+    private String categoria;
+    private String color;
+    private double precio;
+    private int stock;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTalla() {
+        return talla;
+    }
+
+    public void setTalla(String talla) {
+        this.talla = talla;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+    
     
 }
